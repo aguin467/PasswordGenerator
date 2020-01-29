d825a5809342e0e5534f5446d00a136b21b384f9
aguin467 <a.guin003@gmail.com>
2020-01-28 19:25:59 -0500

    Update hash.js

--- js/hash.js
+++ js/hash.js
@@ -13,19 +13,3 @@ let hash = crypto.getHashes();
 let hashPwd = crypto.createHash('sha1').update(characters).digest('hex');
 
 export {firDict, characters, charactersLength};
-
-
-
-
-//const crypto = require('crypto');
-//
-//
-//let hash = crypto.getHashes();
-//
-//
-//let x = 'Geek';
-//
-//
-//let hashPwd = crypto.createHash('sha1').update(x).digest('hex');
-//
-//console.log(hashPwd);
